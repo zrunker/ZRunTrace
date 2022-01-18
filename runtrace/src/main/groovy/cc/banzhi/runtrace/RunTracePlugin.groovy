999768a16eb682533217ee281787ac691ace4c34
zrunker <1823132505@qq.com>
2022-01-18 23:30:30 +0800

    init Commit

--- runtrace/src/main/groovy/cc/banzhi/runtrace/RunTracePlugin.groovy
+++ runtrace/src/main/groovy/cc/banzhi/runtrace/RunTracePlugin.groovy
@@ -1,5 +1,8 @@
 package cc.banzhi.runtrace
 
+import cc.banzhi.runtrace.transforms.RunTraceTransform
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -8,10 +11,15 @@ class RunTracePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.
-        // 定义名RunTraceTask的任务
-        project.task("RunTraceTask") {
-            printf("自定义插件，执行RunTraceTask")
-        }
+//        // 定义名RunTraceTask的任务
+//        project.task("RunTraceTask") {
+//            printf("自定义插件，执行RunTraceTask")
+//        }
+
+        // 注册Transform任务
+        // AppExtension <- AbstractAppExtension <- TestedExtension <- BaseExtension
+        // 这里直接使用BaseExtension也是可以的
+        AppExtension app = project.extensions.getByType(AppExtension)
+        app.registerTransform(new RunTraceTransform())
     }
 }
