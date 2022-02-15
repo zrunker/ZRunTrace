4b5775a4269b92444626fce5e657c3ee692794df
zrunker <1823132505@qq.com>
2022-02-15 19:54:06 +0800

    init Commit

--- runtrace/src/main/groovy/cc/banzhi/runtrace/RunTracePlugin.groovy
+++ runtrace/src/main/groovy/cc/banzhi/runtrace/RunTracePlugin.groovy
@@ -2,6 +2,7 @@ package cc.banzhi.runtrace
 
 import cc.banzhi.runtrace.transform.RunTraceTransform
 import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.internal.plugins.AppPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -10,11 +11,13 @@ class RunTracePlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        // 注册Transform任务
-        BaseExtension android = project.extensions.getByType(BaseExtension)
-        // BaseExtension内部维护着_transforms集合，registerTransform是将TraceTransform实例添加到集合内
-        if (android != null) {
-            android.registerTransform(new RunTraceTransform())
+        if (project.plugins.hasPlugin(AppPlugin)) {
+            // 注册Transform任务
+            BaseExtension android = project.extensions.getByType(BaseExtension)
+            // BaseExtension内部维护着_transforms集合，registerTransform是将TraceTransform实例添加到集合内
+            if (android != null) {
+                android.registerTransform(new RunTraceTransform())
+            }
         }
     }
 }
